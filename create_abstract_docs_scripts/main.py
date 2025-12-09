a5fa7eb9fc4a90d447d251c93003437991c712fe
lizaoh <45185670+lizaoh@users.noreply.github.com>
2025-12-08 16:16:27 -0800

    fixing some typos

--- create_abstract_docs_scripts/main.py
+++ create_abstract_docs_scripts/main.py
@@ -5,7 +5,7 @@ Using Google Drive API to create a Google Doc for each paper's abstract for a sp
 conference. This creates each doc one at a time for now because the abstract text extraction for 1999
 doesn't automate well.
 
-Learned to use API through tutorials by Jie Jann:
+Learned to use API through tutorials by Jie Jenn:
 https://learndataanalysis.org/google-drive-api-in-python-getting-started-lesson-1/#google_vignette
 https://www.youtube.com/watch?v=9K2P2bWEd90&list=PL3JVwFmb_BnTamTxXbmlwpspYdpmaHdbz
 """
@@ -19,8 +19,8 @@ def main():
 
     year = '1999'
     first_author = 'Townsend'
-    titleword = 'Hazard'
-    file_name = f"{year}-{first_author}-{titleword}"
+    title_word = 'Hazard'
+    file_name = f"{year}-{first_author}-{title_word}"
     text_content = 'A new type of hazard function, based on CDFs rather than survivor functions, is proposed. The integrated version is likewise analogous to the traditional integrated hazard function. Just as the traditional integrated hazard function leads to a useful capacity function in the case of minimum-time (i.e., race = OR) processing, the new integrated hazard function permits derivation of a novel capacity function that is appropriate for exhaustive processing experiments.'
 
     print(file_name)
